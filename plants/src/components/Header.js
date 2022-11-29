37b9133628b839f14a9fc12f01771fa7ed4805aa
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-11-29 15:24:13 +0000

    Added basic header format with no search capabilities for now

---
+++ plants/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header-wrapper">
+            <div className="main-info">
+                <h1>Find Plant Care Near You</h1>
+                <h2>Search Form Placeholder</h2>
+                <button className="btn-main-offer">Contact Me</button>
+            </div>
+        </div>
+    )
+}
